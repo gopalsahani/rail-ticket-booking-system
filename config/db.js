ca41a0045739d9cec4d94825159e68c038674d6d
gopalsahani <135799987+gopalsahani@users.noreply.github.com>
2025-02-12 17:48:52 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const mysql = require("mysql2");
 // Create a MySQL connection pool
 const pool = mysql.createPool({
     host: "localhost",
-    user: "root",
-    password: "Gopal@7715",
-    database: "rail",
+    user: "",
+    password: "",
+    database: "",
     waitForConnections: true,
     connectionLimit: 10, // Adjust based on your needs
     queueLimit: 0
