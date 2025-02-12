4f1a4da2ba3beb1b81f9513c0282301f4a9dc02c
gopalsahani <135799987+gopalsahani@users.noreply.github.com>
2025-02-12 17:51:48 +0530

    Update Trainroutes.js

--- routes/Trainroutes.js
+++ routes/Trainroutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { pool } = require(".config/db");
+const { pool } = require("../config/db");
 const jwt = require("jsonwebtoken");
 
 const router = express.Router();
